fd6a74e411bc776275add14e2e02a84a49dc74b1
uletin <dokterkoding111@gmail.com>
2023-01-03 11:11:16 +0800

    fix style

--- pages/blog/react/belajar-react-mulai-darimana.js
+++ pages/blog/react/belajar-react-mulai-darimana.js
@@ -1,7 +1,15 @@
-export default function blogReact1(){
-    return(
-        <div>
-            Belajar React Mulai Darimana ?
-        </div>
-    )
+export default function blogReact1() {
+  return (
+    <>
+      <Head>
+        <title>Belajar React Mulai Darimana Bang?</title>
+        <meta name="description" content="belajar react mulai darimana bang" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={styles.main}>
+        <div>Belajar React Mulai Darimana Bang?</div>
+      </main>
+    </>
+  );
 }
